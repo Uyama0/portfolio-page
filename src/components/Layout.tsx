eb6521080be15e94a3205191d8974b432c78eae6
Ruslan <axelezgames@gmail.com>
2023-10-21 21:34:04 +0500

    add: better blob

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,10 +5,10 @@ import { Header } from "./header/Header";
 export const Layout = () => {
   return (
     <div>
-      {/* <CursorBlob /> */}
+      <CursorBlob />
       <div className="px-4 pb-6 md:p-0 md:max-w-5xl w-full mx-auto flex flex-col">
         <Header />
-        <div className="h-[calc(100vh-77px)]">
+        <div className="">
           <About />
         </div>
         dwq
--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,9 +3,12 @@ import Miyamoto2 from "../../assets/vagabond.jpg";
 
 export const About = () => {
   return (
-    <div className="md:flex flex-1">
-      <div className="md:flex-1"></div>
-      <div className="md:flex-1 flex flex-col gap-6">
+    <div className="flex flex-col md:flex-row flex-1 h-[calc(100vh-77px)]">
+      <div className="flex-1"></div>
+      <div
+        id="right"
+        className="flex-1 flex flex-col gap-6 justify-center md:items-start"
+      >
         <img
           className="max-w-[360px] h-60 md:max-w-sm md:h-64 rounded-xl object-cover"
           src={Miyamoto1}
