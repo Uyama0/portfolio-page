6c79c7016361f029b1a5cfacfb56c4d6b4ac8e16
Ruslan <axelezgames@gmail.com>
2023-11-02 20:32:52 +0500

    add: reveal animation utility

---
+++ src/utilities/framerVariants/framerAnimation.js
@@ -0,0 +1,52 @@
+export const bottomRevealAnimation = {
+  hidden: {
+    y: -100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+export const topRevealAnimation = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+export const leftRevealAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+
+export const rightRevealAnimation = {
+  hidden: {
+    x: 100,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+
+export const fadeInAnimation = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
