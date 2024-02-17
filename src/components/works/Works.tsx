3e40d4a67330267e2e66cded577f9d5f10e62c93
Ruslan <axelezgames@gmail.com>
2024-02-17 17:42:17 +0500

    chore(works): add new works links

--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -5,21 +5,30 @@ export const Works = () => {
     <div id="works" className="grid gap-4 ">
       <div className="grid md:grid-cols-3 md:h-60 gap-4">
         <Card
+          workLink="https://github.com/Uyama0/cross-platform-markdown-editor"
+          workName="markdown editor"
+        />
+        <Card workLink="https://uyama.vercel.app/" workName="portfolio page" />
+        <Card
+          workLink="https://pizza-boot.vercel.app/."
+          workName="pizza boot"
+        />
+      </div>
+      <div className="grid md:grid-cols-2 md:h-80 gap-4">
+        <Card
+          workLink="https://github.com/Uyama0/flow-Field"
+          workName="flow Field"
+        />
+        <Card
           workLink="https://github.com/Uyama0/smooth-animated-todo-app-v2"
           workName="todo v2"
         />
-        <Card
-          workLink="https://github.com/Uyama0/portfolio-page"
-          workName="portfolio page"
-        />
-        <Card />
-      </div>
-      <div className="grid md:grid-cols-2 md:h-80 gap-4">
-        <Card />
-        <Card />
       </div>
       <div className="grid md:grid-cols-3 md:h-60 gap-4">
-        <Card />
+        <Card
+          workLink="https://github.com/Uyama0/message-box"
+          workName="message box"
+        />
         <Card />
         <Card />
       </div>
